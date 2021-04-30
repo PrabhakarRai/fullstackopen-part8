d90d10d1b939e8b52d7685ed68ac17b85a7b2911
Prabhakar Rai <student343.bhu@gmail.com>
2021-04-30 14:31:26 +0530

    8.7 updating born year of author

--- index.js
+++ index.js
@@ -115,6 +115,10 @@ const typeDefs = gql`
       author: String!
       genres: [String!]!
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -179,6 +183,15 @@ const resolvers = {
       }
       return book;
     },
+    editAuthor: (root, args) => {
+      const author = authors.find((a) => a.name === args.name);
+      if (author) {
+        author.born = args.setBornTo;
+        authors = authors.map((a) => a.id === author.id ? author : a);
+        return author;
+      }
+      return null;
+    }
   }
 }
 
