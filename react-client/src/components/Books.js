f114b62d9134619af3da72d6cb69ec6340fd7798
Prabhakar Rai <student343.bhu@gmail.com>
2021-04-30 18:32:14 +0530

    8.9 Books view

--- react-client/src/components/Books.js
+++ react-client/src/components/Books.js
@@ -1,37 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useQuery } from '@apollo/client';
+import { ALL_BOOKS } from '../queries';
 
 const Books = (props) => {
+  const [books, setBooks] = useState(null);
+  const result = useQuery(ALL_BOOKS);
+
+  useEffect(() => {
+    if (result.data) {
+      setBooks(result.data.allBooks);
+    }
+  }, [result]);
+
+
   if (!props.show) {
     return null
   };
 
-  const books = [];
-
+  if (result.loading) {
+    return (
+      <div>
+        <h2>Books</h2>
+        <p>loading books ...</p>
+      </div>
+    );
+  }
+  if (books) {
+    return (
+      <div>
+        <h2>Books</h2>
+        <table>
+          <tbody>
+            <tr>
+              <th>
+                Book Title
+              </th>
+              <th>
+                Author
+              </th>
+              <th>
+                Published
+              </th>
+            </tr>
+            {books.map(a =>
+              <tr key={a.id}>
+                <td>{a.title}</td>
+                <td>{a.author}</td>
+                <td>{a.published}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
   return (
     <div>
       <h2>Books</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th>
-              Book Title
-            </th>
-            <th>
-              Author
-            </th>
-            <th>
-              Published
-            </th>
-          </tr>
-          {books.map(a =>
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+      <p>Unable to load books.</p>
     </div>
   )
 }
